9315739d60796b73afb7f3cd699f735ae413f821
gutierrezashleyy03-cmd <gutierrez.ashleyy03@gmail.com>
2026-09-19 00:46:12 -0700

    Delete public directory

--- public/device/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-header('HTTP/1.1 301 Moved Permanently');
-header('Location: https://oauth.net/2/grant-types/device-code/');
--- public/workshop/next/index.php
+++
@@ -1,2 +0,0 @@
-<?php
-header('Location: https://events.oauth.net/');
